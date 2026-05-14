6bdbc7324916daef9e6de496db3cc2737b66c713
sunoojin <sunoojin17@gmail.com>
2026-05-14 21:46:01 +0900

    ✨ Add pagination to ProductRepository

--- api/src/main/java/com/capstone/triplea/product/ProductRepository.java
+++ api/src/main/java/com/capstone/triplea/product/ProductRepository.java
@@ -1,8 +1,12 @@
 package com.capstone.triplea.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // 기본 CRUD (save, findById, findAll, deleteById)는 JpaRepository가 제공
-    // [TODO] 검색 기능은 나중에 추가
+    // ✅ Pageable을 받으면 정렬 + 페이지네이션 모두 처리 가능
+    // 현재는 정렬만 사용. 2단계(검색)에서 keyword 파라미터 추가 예정
+    Page<Product> findAll(Pageable pageable);
 }
