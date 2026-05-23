cc6ffa3bb2050234ad773f1e137cbc0b0b317d1a
homubee <83688807+homubee@users.noreply.github.com>
2026-05-24 01:21:39 +0900

    ✨ Add discord message for sns publishing #17

--- agent/app/api/v1/router.py
+++ agent/app/api/v1/router.py
@@ -28,6 +28,7 @@ class AgentEventRequest(BaseModel):
 
 @router.post("/agent")
 async def start_agent_flow(body: AgentEventRequest):
+    # 이벤트 수신 메시지 출력
     discord_send_message(
         "🚀 Agent Automation Flow Started",
         f"- {body.event_type} 유형 이벤트 수신\n- productId: {body.product_id}",
@@ -35,6 +36,13 @@ async def start_agent_flow(body: AgentEventRequest):
         7855479
     )
     ai_response: str = await product_marketing(body.event_type, body.is_sample, body.product_new, body.product_old)
+    # SNS 발행 메시지 출력
+    discord_send_message(
+        "📢 SNS Publishing Completed",
+        ai_response,
+        "https://github.com/CSID-DGU/2026-1-CECD1-3-tripleA-07",
+        9109759
+    )
     # 임시 return 값
     return {
         "eventType": body.event_type,
