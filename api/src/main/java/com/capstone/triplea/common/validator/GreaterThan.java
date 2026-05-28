3ca937aa19bf76cb762ed03f3f6fb6c6a24a7bbe
sunoojin <sunoojin17@gmail.com>
2026-05-28 13:35:25 +0900

    🔥 Remove the List for repeatable

--- api/src/main/java/com/capstone/triplea/common/validator/GreaterThan.java
+++ api/src/main/java/com/capstone/triplea/common/validator/GreaterThan.java
@@ -8,17 +8,10 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = GreaterThanValidator.class)
-@Repeatable(GreaterThan.List.class)
 public @interface GreaterThan {
     String message() default "invalid";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
     String field();     // 큰 값이어야 하는 필드 (listPrice)
     String comparedField(); // 작은 값이어야 하는 필드 (price)
-
-    @Target(ElementType.TYPE)
-    @Retention(RetentionPolicy.RUNTIME)
-    @interface List {
-        GreaterThan[] value();
-    }
 }
