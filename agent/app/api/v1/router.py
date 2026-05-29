0e6c38cf943bff5c59ba753a72eed33148afa31c
homubee <83688807+homubee@users.noreply.github.com>
2026-05-30 01:41:45 +0900

    🔥 Remove old api endpoint #23

--- agent/app/api/v1/router.py
+++ agent/app/api/v1/router.py
@@ -31,34 +31,6 @@ class ReviewRequest(AgentEventRequest):
     # 마케팅 json 데이터
     marketing_json: str = Field(alias="marketingJson")
 
-# TODO: 백엔드 마이그레이션 이후 제거
-# 백엔드를 위해 기존 엔드포인트 유지함
-@router.post("")
-async def start_agent_flow_old(body: AgentEventRequest):
-    # 이벤트 수신 메시지 출력
-    discord_send_message(
-        "🚀 Agent Automation Flow Started",
-        f"- {body.event_type} 유형 이벤트 수신\n- productId: {body.product_id}",
-        "https://github.com/CSID-DGU/2026-1-CECD1-3-tripleA-07",
-        7855479
-    )
-    ai_response: str = await product_marketing(body.event_type, body.is_sample, body.product_new, body.product_old)
-    # SNS 발행 메시지 출력
-    discord_send_message(
-        "📢 SNS Publishing Completed",
-        ai_response,
-        "https://github.com/CSID-DGU/2026-1-CECD1-3-tripleA-07",
-        9109759
-    )
-    # 임시 return 값
-    return {
-        "eventType": body.event_type,
-        "productId": body.product_id,
-        "productNew": body.product_new,
-        "productOld": body.product_old,
-        "aiResponse": ai_response
-    }
-
 @router.post("/marketing")
 async def start_agent_flow(body: AgentEventRequest):
     # 이벤트 수신 메시지 출력
