63cbbeda5fb5f0a247349caeddf19fd28b5cbcda
apri1f00ls <wkdduddn7321@gmail.com>
2026-05-31 19:41:33 +0900

    ✨ add form validation condition
    정가 1 이상, 상품 설명 1500자 이하 조건 추가

--- client/src/types/productSchema.ts
+++ client/src/types/productSchema.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 export const productSchema = z.object({
   name: z.string().min(1, "상품명을 입력해주세요."),
   imageUrl: z.string().url({message : "유효한 url을 입력해주세요"}).or(z.literal("")),
-  listPrice: z.number({ 
+  listPrice: z.number({
     error: "숫자를 입력해 주세요."
-  }).nonnegative("정가는 0 이상이어야 합니다."),
+  }).min(1, "정가는 1 이상이어야 합니다."),
   price: z.number({
     error: "숫자를 입력해 주세요."
   }).nonnegative("판매가는 0 이상이어야 합니다."),
@@ -13,7 +13,7 @@ export const productSchema = z.object({
   quantity: z.number({
     error: "숫자를 입력해 주세요."
   }).int().nonnegative("수량은 0 이상의 정수여야 합니다."),
-  description: z.string(),
+  description: z.string().max(1500, "상품 설명은 1500자 이하로 입력해주세요."),
 });
 
 export type ProductFormValues = z.infer<typeof productSchema>;
